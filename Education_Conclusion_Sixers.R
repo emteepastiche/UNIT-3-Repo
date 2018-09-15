f115ed07cb6d857a8625fef467314beffd2c6856
emteepastiche <43260978+emteepastiche@users.noreply.github.com>
2018-09-15 16:30:12 -0500

    Update Education_Conclusion_Sixers.R

--- Education_Conclusion_Sixers.R
+++ Education_Conclusion_Sixers.R
@@ -1,4 +1,4 @@
 data <- read.csv(url('https://raw.githubusercontent.com/emteepastiche/UNIT-3-Repo/master/EducationData.csv'))
 t.test(Income2005~factor(Educ), data = data, alternative ="greater")
 
-#Fail to reject null hypothesis
+#Fail to reject null hypothesis(true mean (12year-16year) is not greater than 0)
